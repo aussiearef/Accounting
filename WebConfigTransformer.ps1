3a862074d0e593f75b6f62289252ad7ad9075f39
Aref Karimi <aref.karimi@just-eat.com>
2018-01-04 08:17:13 +1100

    Adding powershell script for config teansformation

---
+++ WebConfigTransformer.ps1
@@ -0,0 +1,28 @@
+﻿	Param(
+        
+        [String] $solution_dir ="c:\source\Accounting" , 
+        [String] $project_dir ="c:\source\Accounting\Accounting",
+        [String] $output_path = "c:\buildOutput"
+        )
+       
+    $webconfig = $project_dir+"\web.config"
+
+    Write-Output $solution_dir
+
+    Add-Type -LiteralPath "$solution_dir\packages\MSBuild.Microsoft.VisualStudio.Web_WebApplication.Targets.12.0.2\tools\VSToolsPath\Web\Microsoft.Web.XmlTransform.dll"
+
+    
+    New-Item -ItemType directory -Path "$output_path\TransformedConfigs" -Force
+
+	ls $project_dir -Filter "Web.*.config" -Exclude "web.config" ,"web.release.config", "web.debug.config" -Recurse |% {
+
+
+         $xmldoc = New-Object Microsoft.Web.XmlTransform.XmlTransformableDocument;
+    	 $xmldoc.PreserveWhitespace = $true
+    	 $xmldoc.Load($webconfig);
+
+         Write-Output "Transforming configuration file: " + $_.Fullname
+
+    	 $transform = New-Object Microsoft.Web.XmlTransform.XmlTransformation($_.Fullname);
+    	 $xmldoc.Save("$output_path\TransformedConfigs\"+$_.Name)
+    }

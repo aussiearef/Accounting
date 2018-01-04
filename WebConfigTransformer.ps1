b0b3265ca34fddd614dfb740cec27eaeb3be635c
Aref Karimi <aref.karimi@just-eat.com>
2018-01-04 21:25:04 +1100

    Fixed a bug in powershell

--- WebConfigTransformer.ps1
+++ WebConfigTransformer.ps1
@@ -7,10 +7,9 @@
        
     $webconfig = $project_dir+"\web.config"
 
-    Write-Output $solution_dir
+    Add-Type -LiteralPath "$solution_dir\packages\MSBuild.Microsoft.VisualStudio.Web.targets.14.0.0.3\tools\VSToolsPath\Web\Microsoft.Web.XmlTransform.dll"
 
-    Add-Type -LiteralPath "$solution_dir\packages\MSBuild.Microsoft.VisualStudio.Web_WebApplication.Targets.12.0.2\tools\VSToolsPath\Web\Microsoft.Web.XmlTransform.dll"
-
+    
     New-Item -ItemType directory -Path "$output_path\TransformedConfigs" -Force
 
 	ls $project_dir -Filter "Web.*.config" -Exclude "web.config" ,"web.release.config", "web.debug.config" -Recurse |% {
@@ -19,10 +18,11 @@
          $xmldoc = New-Object Microsoft.Web.XmlTransform.XmlTransformableDocument;
     	 $xmldoc.PreserveWhitespace = $true
     	 $xmldoc.Load($webconfig);
+    
 
-         Write-Output "Transforming configuration file: " + $_.Fullname
+         Write-Output "Transforming configuration file: " $_.Fullname
 
     	 $transform = New-Object Microsoft.Web.XmlTransform.XmlTransformation($_.Fullname);
-
+         $transform.Apply($xmldoc);
     	 $xmldoc.Save("$output_path\TransformedConfigs\"+$_.Name)
     }

2a9cc41ae78fc8c14ac9d304091331e4474496c2
Aref Karimi <aref.karimi@just-eat.com>
2018-01-04 21:01:22 +1100

    Added powershell script for config transform

--- WebConfigTransformer.ps1
+++ WebConfigTransformer.ps1
@@ -9,9 +9,8 @@
 
     Write-Output $solution_dir
 
-    Add-Type -LiteralPath "$solution_dir\packages\MSBuild.Microsoft.VisualStudio.Web.targets.14.0.0.3\tools\VSToolsPath\Web\Microsoft.Web.XmlTransform.dll"
+    Add-Type -LiteralPath "$solution_dir\packages\MSBuild.Microsoft.VisualStudio.Web_WebApplication.Targets.12.0.2\tools\VSToolsPath\Web\Microsoft.Web.XmlTransform.dll"
 
-    
     New-Item -ItemType directory -Path "$output_path\TransformedConfigs" -Force
 
 	ls $project_dir -Filter "Web.*.config" -Exclude "web.config" ,"web.release.config", "web.debug.config" -Recurse |% {
@@ -24,5 +23,6 @@
          Write-Output "Transforming configuration file: " + $_.Fullname
 
     	 $transform = New-Object Microsoft.Web.XmlTransform.XmlTransformation($_.Fullname);
+
     	 $xmldoc.Save("$output_path\TransformedConfigs\"+$_.Name)
     }
